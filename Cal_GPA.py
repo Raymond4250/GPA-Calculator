b79c2df751f0e3743b7627d31e5e1e576266fce3
Raymond4250 <reborn12844@hotmail.com>
2019-01-15 23:03:46 +0700

    Add edit mode and refactoring code

--- Cal_GPA.py
+++ Cal_GPA.py
@@ -3,11 +3,9 @@ import math
 
 filename = 'D:\Works\Database Systems\Code\Grade.csv'
 data = []
-grade = []
-credit = []
-score = []
 
 def main():
+  read_data()
   print('''Mode
   1 = Calulate GPA
   2 = Insert
@@ -15,79 +13,60 @@ def main():
   mode = input('Enter mode: ')
 
   if mode == '1':
-    x = input('Enter year: ')
-    y = input('Enter semeter: ')
-    read_data(x,y)
-    display_subject()
-    chage_grade_to_number()
-    cal_score()
-    cal_GPA()
+    select_data = []
+    year = input('Enter year: ')
+    semeter = input('Enter semeter: ')
+    for subject in data:
+      if subject[0] == year and subject[1] == semeter:
+        select_data.append(subject)
+    display_subject(select_data)
+    cal_GPA(select_data)
     print()
   elif mode == '2':
-    print('Insert Mode')
     insert()
+    print()
+  elif mode == '3':
+    edit()
+    print()
 
-def read_data(x,y):
-  '''Read data by select year and term then keep it to list'''
+def read_data():
+  '''Read all data'''
   data.clear()
   with open(filename, newline='') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
-      if row[0] == x and row[1] == y:
-        data.append(row)
+      data.append(row)
     # print(data)
   return data
 
-def cal_GPA():
+def cal_GPA(data):
   '''This function calculate GPA'''
+  print('Calculate GPA Mode')
+  score, credit = cal_score(data)
   total_credit = sum(credit)
   total_score = sum(score)
   GPA = total_score/total_credit
-  GPA = round_down(GPA, 2)
+  GPA = math.floor(GPA * 100) / 100
   print()
   print("GPA: ",GPA)
 
-def cal_score():
+def cal_score(data):
   '''This function calculate score each subject'''
-  global credit
-
-  credit.clear()
-  score.clear()
-
+  credit = []
+  score = []
   for row in data:
     credit.append(row[4])  # Keep only credit for calculate
   credit = list(map(int, credit))  # Convert string to integer
-
   for i in range(len(credit)):
-    score.append(credit[i]*grade[i])
+    score.append(credit[i]*chage_grade_to_number(data[i][5]))
+  return score, credit
 
-  return score
-
-def chage_grade_to_number():
+def chage_grade_to_number(int):
   '''This function change grade form alphabet to numeric and then keep it to list'''
-  grade.clear()
-  for row in data:
-    if row[5] == "A":
-      grade.append(4)
-    elif row[5] == "B+":
-      grade.append(3.5)
-    elif row[5] == "B":
-      grade.append(3)
-    elif row[5] == "C+":
-      grade.append(2.5)
-    elif row[5] == "C":
-      grade.append(2)
-    elif row[5] == "D+":
-      grade.append(1.5)
-    elif row[5] == "D":
-      grade.append(1)
-  return grade
+  grade = {'A': 4, 'B+': 3.5, 'B': 3, 'C+': 2.5,'C': 2, 'D+': 1.5, 'D': 1, 'F': 0}
+  return grade[int]
 
-def round_down(n, decimals=0):
-  multiplier = 10 ** decimals
-  return math.floor(n * multiplier) / multiplier
-
-def display_subject():
+def display_subject(data):
   print()
   print("{:5}|{:8}|{:11}|{:40}|{:8}|{:8}".format('Year', 'Semeter', 'Subject ID', 'Subject', 'Credit', 'Grade'))
   print('-'*80)
@@ -96,6 +75,7 @@ def display_subject():
   print('-'*80)
 
 def insert():
+  print('Insert Mode')
   '''This function add grade into the next row in the same file'''
   with open(filename, 'a', newline='') as csvfile:
     fieldnames = ['Year', 'Semeter', 'Subject ID', 'Subject', 'Credit', 'Grade']
@@ -107,6 +87,34 @@ def insert():
     credit = input('Enter credit: ')
     grade = input('Enter grade: ')
     writer.writerow({'Year': year, 'Semeter': semeter, 'Subject ID': subject_id, 'Subject': subject, 'Credit': credit, 'Grade': grade})
+  print('Save finished!!!')
+
+def edit():
+  print('Edit Mode')
+  year = input('Enter year you need to edit: ')
+  semeter = input('Enter semeter you need to edit: ')
+  select_data = []
+  for subjects in data:
+      if subjects[0] == year and subjects[1] == semeter:
+        select_data.append(subjects)
+  display_subject(select_data)
+
+  old_subject_id = str(input('Enter subject id your need to edit: '))
+  for subject in data:
+    if subject[2] == old_subject_id:
+      subject[2] = input('Enter new subject id: ')
+      subject[3] = input('Enter new subject: ')
+      subject[4] = input('Enter new credit: ')
+      subject[5] = input('Enter new grade: ')
+  # print(data)
+  save_data(data)
+  print('Save finished!!!')
+
+def save_data(data):
+  '''Save new data to CSVfile'''
+  with open(filename, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(data)
 
 while True:
   main()
